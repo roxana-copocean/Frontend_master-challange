4c3cbaf4d3aa9a0ba86514e35ce503412db8576a
Roxana Copocean <roxana.mica@gmail.com>
2022-10-05 11:37:12 +0300

    the numbered list

---
+++ main.js
@@ -0,0 +1,11 @@
+const primaryHeader = document.querySelector('.primary-header');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+	primaryNav.hasAttribute('data-visible')
+		? navToggle.setAttribute('aria-expended', false)
+		: navToggle.setAttribute('aria-expended', true);
+	primaryNav.toggleAttribute('data-visible');
+	primaryHeader.toggleAttribute('data-overlay');
+});
